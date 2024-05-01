77c1f7620eb59fb5da3afad574b40edcd1b4474f
dainavi.purav <dainavi.purav@cmss.in>
2024-05-01 23:44:59 +0530

    get service call added for loading items

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/screens/new_item_screen.dart';
+import 'package:http/http.dart' as http;
 
 import '../widgets/grocery_list.dart';
 
@@ -12,10 +16,42 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  void _loadItems() async {
+    final uri = Uri.https(
+      'flutter-shopping-list-ap-a3013-default-rtdb.firebaseio.com',
+      'shopping-list.json',
+    );
+    final response = await http.get(uri);
+    final Map<String, dynamic> listData = json.decode(response.body);
+
+    List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+            (element) => element.value.name == item.value['category'],
+          )
+          .value;
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+
+    setState(() {
+      _groceryItems = loadedItems;
+    });
+
+    print(response.body);
+  }
 
   void _addNewItem() async {
-    final newItem = await Navigator.push(
+    await Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) {
@@ -24,13 +60,13 @@ class _CategoryScreenState extends State<CategoryScreen> {
       ),
     );
 
-    if (newItem == null) {
-      return;
-    }
+    _loadItems();
+  }
 
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+  @override
+  void initState() {
+    _loadItems();
+    super.initState();
   }
 
   @override
